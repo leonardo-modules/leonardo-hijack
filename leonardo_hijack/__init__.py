d3443eb5be9721494083efe227cac141eb6fe22e
Michael Kutý <6du1ro.n@gmail.com>
2016-04-13 12:21:24 +0200

    Small cleanup.

--- leonardo_hijack/__init__.py
+++ leonardo_hijack/__init__.py
@@ -5,18 +5,20 @@ from django.apps import AppConfig
 default_app_config = 'leonardo_hijack.Config'
 
 
-LEONARDO_URLS_CONF = "hijack.urls"
-
 LEONARDO_APPS = [
-	'leonardo_hijack',
-	"hijack",
-	"compat"
+    'leonardo_hijack',
+    "hijack",
+    "compat"
 ]
 
 LEONARDO_MIDDLEWARES = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'hijack.middleware.HijackRemoteUserMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
+#    'django.contrib.auth.middleware.RemoteUserMiddleware',
+]
+
+LEONARDO_CSS_FILES = [
+    'hijack/hijack-styles.css'
 ]
 
 
--- leonardo_hijack/settings.py
+++ leonardo_hijack/settings.py
@@ -1,3 +1,5 @@
 
 HIJACK_LOGIN_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after hijacking the user.
 HIJACK_LOGOUT_REDIRECT_URL = "/admin/"  # where you want to be redirected to, after releasing the user.
+
+HIJACK_USE_BOOTSTRAP = True
